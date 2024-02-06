aabf4194809012373f289c449930302b5054b1af
Lexe <ngocle.ng1001@gmail.com>
2024-02-06 10:02:29 +0700

    feat: implement auth

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,13 +2,18 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
+import { SessionProvider } from "next-auth/react";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
 };
 
 export default Providers;
---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import React from "react";
+
+const Profile = () => {
+  const session = useSession();
+
+  if (session.data?.user) {
+    return <div>From client: {JSON.stringify(session.data)}</div>;
+  }
+
+  return <div>From client: user is NOT signed in</div>;
+};
+
+export default Profile;
