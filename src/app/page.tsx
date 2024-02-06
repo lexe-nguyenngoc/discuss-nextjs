b07c0fe60f8ac78b9b89ec8231471f9b4490f612
Lexe <ngocle.ng1001@gmail.com>
2024-02-06 16:56:03 +0700

    feat: implement topic list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import TopicCreateForm from "@/components/topics/TopicCreateForm";
+import TopicList from "@/components/topics/TopicList";
+import { Divider } from "@nextui-org/react";
 
 const HomePage = () => {
   return (
@@ -8,8 +10,11 @@ const HomePage = () => {
       <div className="col-span-3">
         <h1 className="text-xl m-2">Top Posts</h1>
       </div>
-      <div>
+      <div className="border shadow py-3 px-2">
         <TopicCreateForm />
+        <Divider className="my-2" />
+        <h3 className="text-lg">Topics</h3>
+        <TopicList />
       </div>
     </div>
   );
---
+++ src/components/topics/TopicList/index.tsx
@@ -0,0 +1,26 @@
+import { Chip } from "@nextui-org/react";
+import React from "react";
+
+import db from "@/db";
+import paths from "@/paths";
+import Link from "next/link";
+
+const TopicList = async () => {
+  const topics = await db.topic.findMany();
+
+  const renderedTopics = topics.map((topic) => {
+    return (
+      <div key={topic.id}>
+        <Link href={paths.topicShow(topic.slug)}>
+          <Chip color="warning" variant="shadow">
+            {topic.slug}
+          </Chip>
+        </Link>
+      </div>
+    );
+  });
+
+  return <div className="flex flex-row gap-2 flex-wrap">{renderedTopics}</div>;
+};
+
+export default TopicList;
