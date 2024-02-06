e24379be317cd23c1e597950bfd5cb5b3efc4398
Lexe <ngocle.ng1001@gmail.com>
2024-02-06 10:36:05 +0700

    feat: create server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,4 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export const signIn = async () => {
-  return auth.signIn("github");
-};
-
-export const signOut = async () => {
-  return auth.signOut();
-};
+export { signIn, signOut } from "./auth";
+export { createComment } from "./comment";
+export { createPost } from "./post";
+export { createTopic } from "./topic";
