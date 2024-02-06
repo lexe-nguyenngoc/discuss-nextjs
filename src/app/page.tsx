67a4300d97f4145692a2eb87d56f69b84313c2cf
Lexe <ngocle.ng1001@gmail.com>
2024-02-06 15:04:35 +0700

    feat: implement Create a Topic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import TopicCreateForm from "@/components/TopicCreateForm";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
