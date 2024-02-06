1ccd5662e5b863cbf956aef6bd0b014259dfdbd8
Lexe <ngocle.ng1001@gmail.com>
2024-02-06 16:06:44 +0700

    feat: implement validate topic create form

--- src/actions/topic.ts
+++ src/actions/topic.ts
@@ -1,5 +1,48 @@
 "use server";
+import { auth } from "@/auth";
+import { z } from "zod";
 
-export const createTopic = async () => {
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letter or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export const createTopic = async (
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> => {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return { errors: result.error.formErrors.fieldErrors };
+  }
+
+  const session = await auth();
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  return { errors: {} };
   // TODO: revalidate the home page
 };
--- src/components/TopicCreateForm/index.tsx
+++ src/components/TopicCreateForm/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Button,
   Input,
@@ -7,25 +9,47 @@ import {
   Textarea,
 } from "@nextui-org/react";
 import React from "react";
+import { useFormState } from "react-dom";
 
 import * as actions from "@/actions";
 
 const TopicCreateForm = () => {
+  const [state, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              label="Name"
+              name="name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!state.errors.name}
+              errorMessage={state.errors.name?.[0]}
+            />
             <Textarea
               label="Description"
+              name="description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!state.errors.description}
+              errorMessage={state.errors.description?.[0]}
             />
+
+            {state.errors._form && (
+              <div className="rounded p-2 bg-red-200 border-red-400">
+                {state.errors._form[0]}
+              </div>
+            )}
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
