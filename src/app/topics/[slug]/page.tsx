b8af9e2db91f82de60e8977737bfbe5fb28f9dbd
Lexe <ngocle.ng1001@gmail.com>
2024-02-07 11:01:57 +0700

    feat: implement post list

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+import { fetchPostsByTopicSlug } from "@/db/queries/posts";
+
 import PostCreateForm from "@/components/posts/PostCreateForm";
+import PostList from "@/components/posts/PostList";
 
 interface TopicShowPageProps {
   params: {
@@ -15,6 +18,7 @@ const TopicShowPage = ({ params }: TopicShowPageProps) => {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
 
       <div>
