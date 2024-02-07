6f9a20be307950550f75059579071008f5939870
Lexe <ngocle.ng1001@gmail.com>
2024-02-07 15:38:52 +0700

    feat: implement top posts at home page

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,15 +1,13 @@
 import type { Post } from "@prisma/client";
 import db from "@/db";
 
-// export type PostWithData = Post & {
-//   topic: { slug: string };
-//   user: { name: string | null };
-//   _count: {
-//     comments: number;
-//   };
-// };
-
-export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>;
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: {
+    comments: number;
+  };
+};
 
 export const fetchPostsByTopicSlug = (slug: string) => {
   return db.post.findMany({
@@ -23,5 +21,21 @@ export const fetchPostsByTopicSlug = (slug: string) => {
       user: { select: { name: true } },
       _count: { select: { comments: true } },
     },
+  });
+};
+
+export const fetchTopPosts = async (): Promise<PostWithData[]> => {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: { _count: "desc" },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
   });
 };
